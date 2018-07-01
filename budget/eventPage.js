d9a5307ba5e1d04e543f5639ac4b4a689b55f633
ayush-a <ayushagarwal.march@gmail.com>
2018-07-02 00:20:25 +0530

    Added Context Menu

---
+++ budget/eventPage.js
@@ -0,0 +1,44 @@
+var contextMenuItem = {
+    "id": "spendMoney",
+    "title": "SpendMoney",
+    "contexts": ["selection"]
+};
+chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value){
+    return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "spendMoney" && clickData.selectionText){
+        if (isInt(clickData.selectionText)){
+            chrome.storage.sync.get(['total', 'limit'], function(budget){
+                var newTotal = 0;
+                if (budget.total){
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText)
+                chrome.storage.sync.set({"total": newTotal}, function(){
+                    if (newTotal >= budget.limit){
+                        var limitNotif = {
+                            type: "basic",
+                            iconUrl: "icons8-money-40.png",
+                            title: "Limit Reached",
+                            message: "Oops! You have reached the limit."
+                        };
+                        chrome.notifications.create("limitNotification", limitNotif);
+                    }
+                    else{
+                        var spendNotif = {
+                            type: "basic",
+                            iconUrl: "icons8-money-40.png",
+                            title: "Money spent.",
+                            message: "Looks like you have spent "+ clickData.selectionText +" units of currency."
+                        };
+                        chrome.notifications.create("spendNotification", spendNotif);
+                    }
+                });
+            });
+        }
+    }
+});
